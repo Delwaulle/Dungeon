818511ef9822e11c320f180bffab5cf85d304bf2
fguilbert <fguilbert@192.168.0.10>
2015-09-24 08:58:47 +0200

    Comment the code - Part 4 (dungeon.action)

--- Dungeon/src/dungeon/action/DecorAction.java
+++ Dungeon/src/dungeon/action/DecorAction.java
@@ -4,15 +4,27 @@ public class DecorAction implements Action{
 	private String description;
 	private String consequence;
 	
+	/**
+	 * create a decor and its consequence in a room
+	 * @param description
+	 * @param consequence
+	 */
 	public DecorAction(String description,String consequence){
 		this.setDescription(description);
 		this.setConsequence(consequence);
 	}
 
+	/**
+	 * @return the description of the decor
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * set the description of the decor
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
@@ -21,6 +33,10 @@ public class DecorAction implements Action{
 		return consequence;
 	}
 
+	/**
+	 * set the consequence of the decor
+	 * @param consequence
+	 */
 	public void setConsequence(String consequence) {
 		this.consequence = consequence;
 	}
